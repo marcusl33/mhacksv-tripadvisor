4f4e6f2e4e92258cb05aff1da3ef30f7e4a606dc
penguinsr6 <rsikdar@berkeley.edu>
2015-01-17 11:15:00 -0500

    get all user input

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var app = express();
-var request = require('request');
 // var url = require('url');
 
 
@@ -19,6 +18,13 @@ app.get('/details', function(req, res) {
 	res.sendFile(__dirname + '/details.html');
 });
 
+app.get('/plan', function(req, res) {
+	var places = req.query.list_of_places;
+	var duration = req.query.time_range;
+	res.end(places, duration);
+});
+
+
 app.listen(3000, function(){
 	console.log('listening on port 3000');
 });
@@ -32,53 +38,53 @@ app.listen(3000, function(){
 
 //set var to full day
 
-var itinerary = [];
+// var itinerary = [];
 
 
 
-//place object
-function place(location, duration) {
-	this.location = location;
-	this.duration = duration;
-}
+// //place object
+// function place(location, duration) {
+// 	this.location = location;
+// 	this.duration = duration;
+// }
 
-//convert all places into place objects
+// //convert all places into place objects
 
-function travel(place1, place2, travel_method) {
-		//make call to google api
-		//use callback function
-	if (travel_method === undefined) {
-		travel_method = google.maps.TravelMode.DRIVING;
-	}
+// function travel(place1, place2, travel_method) {
+// 		//make call to google api
+// 		//use callback function
+// 	if (travel_method === undefined) {
+// 		travel_method = google.maps.TravelMode.DRIVING;
+// 	}
 
-	var origin1 = place1.location;
-	var destinationA = place2.location;
+// 	var origin1 = place1.location;
+// 	var destinationA = place2.location;
 
-	var service = new google.maps.DistanceMatrixService();
-	service.getDistanceMatrix(
-	{
-		origins: [origin1, origin1],
-		destinations: [destinationA, destinationA],
-		travelMode: travel_method,
-		unitSystem: UnitSystem,
-		durationInTraffic: Boolean,
-		avoidHighways: false,
-		avoidTolls: false
-	}, callback);
+// 	var service = new google.maps.DistanceMatrixService();
+// 	service.getDistanceMatrix(
+// 	{
+// 		origins: [origin1, origin1],
+// 		destinations: [destinationA, destinationA],
+// 		travelMode: travel_method,
+// 		unitSystem: UnitSystem,
+// 		durationInTraffic: Boolean,
+// 		avoidHighways: false,
+// 		avoidTolls: false
+// 	}, callback);
 
-	function callback(response, status) {
-		if (status != google.maps.DistanceMatrixStatus.OK) {
-			alert('Error was: ' + status);
-		} else {
-		var dist = response.rows[0].elements[0].distance.text;
-		var time = response.rows[0].elements[0].duration.text;
-		}
-	}
-}
+// 	function callback(response, status) {
+// 		if (status != google.maps.DistanceMatrixStatus.OK) {
+// 			alert('Error was: ' + status);
+// 		} else {
+// 		var dist = response.rows[0].elements[0].distance.text;
+// 		var time = response.rows[0].elements[0].duration.text;
+// 		}
+// 	}
+// }
 
-list_of_places.forEach(function(place) {
-	if (place.duration === 'full') {
-		itinerary.push(place);
-	}
-});
+// list_of_places.forEach(function(place) {
+// 	if (place.duration === 'full') {
+// 		itinerary.push(place);
+// 	}
+// });
 
